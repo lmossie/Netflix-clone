b80b822b90c71f162fc1c6946bee9135dcb83fce
lmossie <lamesgin2020@gmail.com>
2024-07-11 11:21:02 -0500

    vite.config.js issue fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ghPages from "vite-plugin-gh-pages";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ghPages()],
-})
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  base: "/Netflix-clone/", // Replace 'Netflix-clone' with your actual repo name
+});
